d04fe037c7c117e851e4a83138ef59f0d1e46b70
Amendoza20 <20.angel.mendoza@gmail.com>
2019-10-29 17:23:58 -0400

    first push added getters and setter

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,51 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    String firstName = "";
+    String lastName = "";
+    ArrayList<Double> testScores = new ArrayList<>();
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+
+    }
+
+    // public Double numberOfExamsTaken(Double[] testScores){
+
+    //}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double> getExamScore() {
+        return testScores;
+    }
+    //public Double getNumberOfExamsTaken(){
+    //    return numberOfExamsTaken;
+    //}
+
+    public String getExamScores() {
+        String result = "";
+        //for(int i = 0; i <)
+        //}
+        return "";
+    }
 }
+
+
+
+
